2c074d88c192fe4b339ce119824513c4a3ec87e3
Chanadu <chandu.peddada@gmail.com>
2024-09-15 17:27:39 -0500

    quick filetype changes that i dont think work

--- lua/chanadu/core/options.lua
+++ lua/chanadu/core/options.lua
@@ -45,3 +45,25 @@ opt.wrap = false
 opt.background = "dark"
 
 opt.pumheight = 10
+if not vim.filetype then
+	return
+end
+
+vim.filetype.add({
+	extension = {
+		lock = "yaml",
+		gotmpl = "gotmpl",
+	},
+	filename = {
+		[".gitignore"] = "conf",
+		[".eslintrc"] = "jsonc",
+		[".prettierrc"] = "jsonc",
+		[".babelrc"] = "jsonc",
+		[".flake8"] = "dosini",
+	},
+	pattern = {
+		[".env.*"] = "sh",
+		["*.Dockerfile.*"] = "dockerfile",
+		["*sway*"] = "swayconfig",
+	},
+})
